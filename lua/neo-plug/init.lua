f71d010aa42fb6fb7e710dca0b9f280d9b282c06
solimanhub <david.soliman2010@gmail.com>
2025-03-12 11:24:10 -0600

    enter mod

--- lua/neo-plug/init.lua
+++ lua/neo-plug/init.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 local function transform_line()
+
     local row = vim.api.nvim_win_get_cursor(0)[1] - 1
     local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
     
-    if line:match("^##ia: ") then
+    if line:match("##ia: ") then
         local text = line:sub(7)
         vim.api.nvim_buf_set_lines(0, row, row + 1, false, {})
         
@@ -20,7 +21,8 @@ end
 function M.setup()
     vim.keymap.set('i', '<CR>', function()
         if not transform_line() then
-            vim.api.nvim_put({''}, 'l', true, true)
+            --vim.api.nvim_put({''}, 'l', true, true)
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', false)
         end
     end, {noremap = true, silent = true})
 end
